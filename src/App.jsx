2cbcc8ff232fbee53ee8dde33873040896253ac8
Daniel Painter <danielpainter@Daniels-iMac.local>
2024-06-16 13:49:37 -0400

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero"
+import KeyFeatures from "./components/KeyFeatures"
+import BassShaker from "./components/BassShaker"
+import NewFeatures from "./components/NewFeatures"
+import Footer from "./components/Footer";
+
+import "./index.css";
+import '../fontawesome'; 
+
+
+
+
 function App() {
 
   return (
-    <div>
-      Hello world!
+    <div className="bg-sm-gradient overflow-x-hidden" >
+      <div>
+        <div className='max-w-7xl m-auto px-8 sm:px-6'>
+          <NavBar />
+          <Hero />
+          <KeyFeatures  />
+          <BassShaker  />
+          <NewFeatures  />
+        </div>
+      </div>
+      <div> 
+        <Footer />
+        </div>
     </div>
   )
 }
